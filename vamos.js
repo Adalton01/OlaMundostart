0323102047942c493635b7bab4dffb2fa47c3286
Dalton <adaltonleal15@gmail.com>
2024-02-20 17:28:14 -0300

    Atualizado Exercicios Vamos

--- vamos.js
+++ vamos.js
@@ -1,50 +1,45 @@
-function calc ()
+function start ()
 {
-    var ini = document.getElementById( "ini" )
-    var fim = document.getElementById( "fim" )
-    var passo = document.getElementById( "passo" )
-    var res = document.getElementById( "res" )
+  var ini = document.getElementById( "ini" )
+  var fim = document.getElementById( "fim" )
+  var pas = document.getElementById( "pas" )
+  var res = document.getElementById( "res" )
 
-    if ( ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 )
+
+  if ( ini.value.length <= 0 || fim.value.length <= 0 || pas.value.length <= 0 )
+  {
+    window.alert( "ERRO..... Digite os numeros" )
+
+  }
+  else
+  {
+    ini1 = Number( ini.value )
+    fim1 = Number( fim.value )
+    pas1 = Number( pas.value )
+
+    if ( ini1 <= fim1 )//contagem crescente 
     {
-        res.innerHTML = "Impossivel contar"
-        // window.alert( "[Erro] Verifique os dados ..." )
+      for ( var c = ini1; c <= fim1; c += pas1 )
+      {
+        res.innerHTML += `  \u{1F604}${ c }`
 
-
-
-    } else
+      }
+    } else // contagem regressiva
     {
-        res.innerHTML = "Contando : "
-        var i = Number( ini.value )
-        var f = Number( fim.value )
-        var p = Number( passo.value )
-
-        if ( p <= 0 )
-        {
-            window.alert( "Passo invalido" )
-            p = 1
-
-        }
+      for ( var c = ini1; c >= fim1; c -= pas1 )
+      {
+        res.innerHTML += `\u{1F643} ${ c }`
 
 
-        if ( i < f )
-        {
-            for ( var c = i; c <= f; c += p )//contagem crescente
-            {
-                res.innerHTML += `\u{1F449} ${ c } `
-
-            }
-        }
-        else
-        {
-            for ( var c = i; c >= f; c -= p )
-            {//contagem decrescente
-                res.innerHTML += `\u{1F449} ${ c } `//adicionando emoji
-
-
-            }
-        }
-
-        res.innerHTML += `\u{1F449} ${ c } `//adicionando 
+      }
     }
+
+
+
+  }
 }
+
+
+
+
+
